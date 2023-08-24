1c40298b0135c9d78a0470757975955a142d7a7f
Pontsho04 <mabopontsho@gmail.com>
2023-08-24 13:54:11 -0700

    0x19. c - stacks queues - lifo fifo

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop - nothing
+ * @counter: line_number
+ * @head: stack head
+ * Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
